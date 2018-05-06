fb43bc4b8f6259c97fcbd5095684e7bd8f2aa311
andiankas <rustiantoandi17@gmail.com>
2018-05-05 17:59:20 -0700

    konstanta

---
+++ 01_basic/02_konstanta.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Hello World</title>
+</head>
+<body>
+
+	<?php 
+
+		
+
+		$nama = "Andi";
+		define("country", "Indonesia");
+
+		echo '$nama  - Kebangsaan: country';
+
+		echo '<br>';
+
+		echo '$nama  - Kebangsaan: $country';
+
+		echo '<br>';
+
+		echo '$nama  - Kebangsaan: '.country;
+
+
+
+
+
+	 ?>
+	
+</body>
+</html>
