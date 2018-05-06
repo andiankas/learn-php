36fbd8b968567a64bce2d6a8062cc22139a5aa2d
andiankas <rustiantoandi17@gmail.com>
2018-05-05 17:52:48 -0700

    first commit

---
+++ 01_basic/01_helloWorld.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Hello World</title>
+</head>
+<body>
+
+	<?php 
+
+		echo 'Hello World';
+
+		echo '<br><br>';
+
+		$variable = "Ini variable";
+		echo $variable;
+
+	 ?>
+	
+</body>
+</html>
